f1a8b13aa5bf9cc178f6e67378c5e7184ed9b6c7
pintuproject <104198297+pintuproject@users.noreply.github.com>
2022-07-16 20:12:11 +0530

    Add files via upload

---
+++ ArithmaticException.java
@@ -0,0 +1,9 @@
+class ArithmaticException{
+public static void main(String args[])
+{
+int a=0;
+int b=1;
+int c=b/a;
+System.out.println(c);
+}
+}
---
+++ Example2.java
@@ -0,0 +1,16 @@
+class Example2 {
+   public static void main(String args[]) {
+      int num1, num2;
+      try {
+         
+         num1 = 0;
+         num2 = 62 / num1;
+         System.out.println(num2);
+         System.out.println("Hey I'm at the end of try block");
+      }
+      catch (ArithmeticException e) { 
+          
+         System.out.println("You should not divide a number by zero");
+      }
+}
+}
